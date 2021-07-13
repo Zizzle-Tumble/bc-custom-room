a078e0712a59913382fcdac2bab366ed1e45d7ea
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2021-07-13 13:04:22 +1200

    Update room.js

--- default-room-test/room.js
+++ default-room-test/room.js
@@ -1,4 +1,9 @@
 newroom = {roomId: "Pizza",  name: "Pizza Place", width: 855, height: 480, startX: 440, startY: 380, startZ: 0, layout: {playground: [[]], height:480, width:885}, media: {background:"https://i.imgur.com/meyqL7K.png" , foreground:"" , music:"https://www.dl.dropboxusercontent.com/s/j30mu8q6f6tvkch/29.%20Charlies%20Here.mp3?dl=1" , navMesh:"https://i.imgur.com/OD7eE16.png", treasure:"https://i.imgur.com/m3YCOSh.png" , }, spritesheet:{}, triggers: [{hex:'ff0000',world:{joinRoom:'port'}}],}
+whitelisted = JSON.parse(localStorage.getItem("whitelist"))
+
+if (whitelisted == undefined || whitelisted == null){
+   whitelisted = []
+}
 
 
 
@@ -19,6 +24,7 @@ joinnewroom()
 
 plzcon = function(){
 joinconf = confirm("Do you want to enter personal room "+newroom.name+"? [Note:Players Can still see what you say!]")
+   alert("NOTE: YOU CAN WHITELIST WHO YOU CAN SEE BY TYPING whitelist(``id of player``) IN THE CONSOLE!")
 if (joinconf == true){
 joinnewroom() 
 playrhide()
@@ -42,7 +48,7 @@ reload = function(){
  playrhide = function(){
  var RoomLoop;
  for (RoomLoop = 0; RoomLoop < world.room.playerCrumbs.length; RoomLoop++){
-     if (world.stage.room.players[world.room.playerCrumbs[RoomLoop].i].visible == true && world.stage.room.players[world.room.playerCrumbs[RoomLoop].i].playerId != world.player.playerId && world.room.roomId == newroom.roomId){
+     if (world.stage.room.players[world.room.playerCrumbs[RoomLoop].i].visible == true && world.stage.room.players[world.room.playerCrumbs[RoomLoop].i].playerId != world.player.playerId && whitelisted.includes([world.room.playerCrumbs[RoomLoop].i].playerId) != true && world.room.roomId == newroom.roomId){
 
       world.stage.room.players[world.room.playerCrumbs[RoomLoop].i].visible = false
       world.stage.room.players[world.room.playerCrumbs[RoomLoop].i].nickname.visible = false
@@ -51,4 +57,9 @@ reload = function(){
  if (world.room.roomId == newroom.roomId){
      reload()
  } 
-} 
+}
+
+function whitelist(playerid){
+   whitelisted.push(playerid)
+   localStorage.setItem("whitelist",JSON.stringify(whitelisted))
+}
