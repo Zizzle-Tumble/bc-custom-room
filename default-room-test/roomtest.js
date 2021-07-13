376feee8431b9486ea009e28b0035a8752da67a5
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2021-07-14 11:59:40 +1200

    Update roomtest.js

--- default-room-test/roomtest.js
+++ default-room-test/roomtest.js
@@ -15,10 +15,11 @@ if(new.target){
   client.loadedData.rooms.push ({baseroom:"shack",roomId: "Pizza",  name: "Pizza Place", width: 855, height: 480, startX: 440, startY: 380, startZ: 0, layout: {playground: [[]], height:480, width:885}, media: {background:"https://i.imgur.com/meyqL7K.png" , foreground:"" , music:"https://www.dl.dropboxusercontent.com/s/j30mu8q6f6tvkch/29.%20Charlies%20Here.mp3?dl=1" , navMesh:"https://i.imgur.com/OD7eE16.png", treasure:"https://i.imgur.com/m3YCOSh.png" , }, spritesheet:{}, triggers: [{hex:'ff0000',world:{joinRoom:'port'}}],})
   world.player.x = world.rooms[world.rooms.length - 1].startX
   world.player.y = world.rooms[world.rooms.length - 1].startY
+  setTimeout(function(){world.handleJoinRoom({roomId:world.rooms[world.rooms.length - 1].roomId, playerCrumbs:[client.makeCrumb(world.player)]})},1000)
   world.joinRoom(world.rooms[world.rooms.length - 1].baseroom)
-  setTimeout(world.handleJoinRoom({roomId:world.rooms[world.rooms.length - 1].roomId, playerCrumbs:[client.makeCrumb(world.player)]}),1500)
-  world.joinRoom(world.rooms[world.rooms.length - 1].roomId)
-  setTimeout(world.handleJoinRoom({roomId:world.rooms[world.rooms.length - 1].roomId, playerCrumbs:[client.makeCrumb(world.player)]}),1500)
+  
+  
+
   
 } else{
 //manage existing room
@@ -42,17 +43,21 @@ if(new.target){
   // (add navmesh, etc later)
     
 }
-    if (world.rooms[r].roomId == world.rooms[world.rooms.length - 1].roomId){
+    if (world.rooms[r].baseroom != undefined || null){
       world.joinRoom(world.rooms[r].baseroom)
-     
-      setTimeout(world.handleJoinRoom({roomId:world.rooms[r].roomId, playerCrumbs:[client.makeCrumb(world.player)]}),1500)
+      setTimeout(function(){world.handleJoinRoom({roomId:world.rooms[r].roomId, playerCrumbs:[client.makeCrumb(world.player)]})},1000)
       
-      world.joinRoom(world.rooms[r].roomId)
-      
-      setTimeout(world.handleJoinRoom({roomId:world.rooms[r].roomId, playerCrumbs:[client.makeCrumb(world.player)]}),1500)
     
     }else{
       world.joinRoom(world.rooms[r].roomId)
     }
+  },
+  JoinRoomByIdNum: function(id){
+    if (world.rooms[id].baseroom != undefined || null){
+      world.joinRoom(world.rooms[id].baseroom)
+      setTimeout(function(){world.handleJoinRoom({roomId:world.rooms[id].roomId, playerCrumbs:[client.makeCrumb(world.player)]})},1000)
+    }else{
+      world.joinRoom(world.rooms[id].roomId)
   }
+ }
 }
