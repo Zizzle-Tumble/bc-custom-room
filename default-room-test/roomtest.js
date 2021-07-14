6b665d2baceff4df9eeac0a411239c63a7ff6cad
Tumble <tumblegamer@gmail.com>
2021-07-14 11:46:31 +0100

    organsised prototypes

--- default-room-test/roomtest.js
+++ default-room-test/roomtest.js
@@ -44,39 +44,47 @@ const BC = (() => {
 
 	})();
 
-
-
-
-	function BCAL_Room(name, roomId) {
+	function BCAL_Room(id, data) {
 		if (new.target) {
-			//create custom room
+			if (data) {
+				data.roomId = id;
 
-			world.rooms.push({ baseroom: "shack", roomId: roomId, name: name, width: 855, height: 480, startX: 440, startY: 380, startZ: 0, layout: { playground: [[]], height: 480, width: 885 }, media: { background: "https://i.imgur.com/meyqL7K.png", foreground: "", music: "https://www.dl.dropboxusercontent.com/s/j30mu8q6f6tvkch/29.%20Charlies%20Here.mp3?dl=1", navMesh: "https://i.imgur.com/OD7eE16.png", treasure: "https://i.imgur.com/m3YCOSh.png", }, spritesheet: {}, triggers: [{ hex: 'ff0000', world: { joinRoom: 'port' } }], })
-			client.loadedData.rooms.push({ baseroom: "shack", roomId: roomId, name: name, width: 855, height: 480, startX: 440, startY: 380, startZ: 0, layout: { playground: [[]], height: 480, width: 885 }, media: { background: "https://i.imgur.com/meyqL7K.png", foreground: "", music: "https://www.dl.dropboxusercontent.com/s/j30mu8q6f6tvkch/29.%20Charlies%20Here.mp3?dl=1", navMesh: "https://i.imgur.com/OD7eE16.png", treasure: "https://i.imgur.com/m3YCOSh.png", }, spritesheet: {}, triggers: [{ hex: 'ff0000', world: { joinRoom: 'port' } }], })
-			world.player.x = world.rooms[world.rooms.length - 1].startX
-			world.player.y = world.rooms[world.rooms.length - 1].startY
-			setTimeout(function () { world.handleJoinRoom({ roomId: world.rooms[world.rooms.length - 1].roomId, playerCrumbs: [client.makeCrumb(world.player)] }) }, 1000)
-			world.joinRoom(world.rooms[world.rooms.length - 1].baseroom)
-			this.numid = world.rooms.length - 1
-			this.id = world.rooms[world.rooms.length - 1].roomId
-			this.baseroom = world.rooms[world.rooms.length - 1].baseroom
-			this.UpdateRoom = function (r, i, l) {
-				BC.API.UpdateRoom(r, i, l)
+				this.id = id;
+				this.data = data;
+
+
+
+				world.rooms.push({ baseroom: "shack", roomId: roomId, name: name, width: 855, height: 480, startX: 440, startY: 380, startZ: 0, layout: { playground: [[]], height: 480, width: 885 }, media: { background: "https://i.imgur.com/meyqL7K.png", foreground: "", music: "https://www.dl.dropboxusercontent.com/s/j30mu8q6f6tvkch/29.%20Charlies%20Here.mp3?dl=1", navMesh: "https://i.imgur.com/OD7eE16.png", treasure: "https://i.imgur.com/m3YCOSh.png", }, spritesheet: {}, triggers: [{ hex: 'ff0000', world: { joinRoom: 'port' } }], })
+				client.loadedData.rooms.push({ baseroom: "shack", roomId: roomId, name: name, width: 855, height: 480, startX: 440, startY: 380, startZ: 0, layout: { playground: [[]], height: 480, width: 885 }, media: { background: "https://i.imgur.com/meyqL7K.png", foreground: "", music: "https://www.dl.dropboxusercontent.com/s/j30mu8q6f6tvkch/29.%20Charlies%20Here.mp3?dl=1", navMesh: "https://i.imgur.com/OD7eE16.png", treasure: "https://i.imgur.com/m3YCOSh.png", }, spritesheet: {}, triggers: [{ hex: 'ff0000', world: { joinRoom: 'port' } }], })
+				world.player.x = world.rooms[world.rooms.length - 1].startX
+				world.player.y = world.rooms[world.rooms.length - 1].startY
+				setTimeout(function () { world.handleJoinRoom({ roomId: world.rooms[world.rooms.length - 1].roomId, playerCrumbs: [client.makeCrumb(world.player)] }) }, 1000)
+				world.joinRoom(world.rooms[world.rooms.length - 1].baseroom)
+				this.numid = world.rooms.length - 1
+				this.id = world.rooms[world.rooms.length - 1].roomId
+				this.baseroom = world.rooms[world.rooms.length - 1].baseroom
 			}
-			this.JoinRoomByIdNum = function (id) {
-				BC.API.JoinRoomByIdNum(id)
-			}
-
-
-
 		} else {
-			//manage existing room
+			//Existing Room
+			if (!data) {
+				//Obtain room data
+				data = {};
+			}
+			return new BCAL_Room(id, data)
 		}
+	}
+
+	BCAL_Room.prototype.updateRoom = function (property, value) {
+		api.updateRoom(this.id, property, value)l
+	}
+
+	BCAL_Room.prototype.joinRoom = function () {
+		api.JoinRoomByIdNum(this.id)
 	}
 
 	return {
 		api,
-		Room: BCAL_Room;
+		Room: BCAL_Room
 	}
 })();
 
