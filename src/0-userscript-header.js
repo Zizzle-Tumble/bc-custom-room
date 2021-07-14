f9563a3e5756bf694e129dd070dd031626ae3193
Tumble <tumblegamer@gmail.com>
2021-07-14 14:04:46 +0100

    gulp build

---
+++ src/0-userscript-header.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         BCAL
+// @namespace    https://boxcrittersmods.ga/authors/zizzle/
+// @supportURL   http://discord.gg/D2ZpRUW
+// @version      0.0.0.1
+// @description  This is a cool mod
+// @author       TumbleGamer,Zizzle
+// @match        https://boxcritters.com/play/
+// @match        https://boxcritters.com/play/?*
+// @match        https://boxcritters.com/play/#*
+// @match        https://boxcritters.com/play/index.html
+// @match        https://boxcritters.com/play/index.html?*
+// @match        https://boxcritters.com/play/index.html#*
+// @run-at       document-start
+// ==/UserScript==
+
+const BC = {};
--- src/api.js
+++ src/api.js
@@ -1,20 +1,3 @@
-// ==UserScript==
-// @name         BCAL
-// @namespace    https://boxcrittersmods.ga/authors/zizzle/
-// @supportURL   http://discord.gg/D2ZpRUW
-// @version      0.0.0.1
-// @description  This is a cool mod
-// @author       TumbleGamer,Zizzle
-// @match        https://boxcritters.com/play/
-// @match        https://boxcritters.com/play/?*
-// @match        https://boxcritters.com/play/#*
-// @match        https://boxcritters.com/play/index.html
-// @match        https://boxcritters.com/play/index.html?*
-// @match        https://boxcritters.com/play/index.html#*
-// @run-at       document-start
-// ==/UserScript==
-
-const BC = {};
 BC.api = (() => {
 
 	function updateRoom(r, i, l) {
