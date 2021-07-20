8ee3f6d2d0516933523eccfd6753df0f99afe335
ZizzleBCM <59903175+zizzle8383@users.noreply.github.com>
2021-07-20 12:03:02 +1200

    add bot

--- dist/bcal.js
+++ dist/bcal.js
@@ -72,13 +72,25 @@ BC.Room = (() => {
 				this.id = id;
 				this.data = data;
 
-
+                                
+				
+			        
+				crumby = {
+    					"i": "test",
+   					"c": "beaver",
+    					"n": "test",
+   				        "g": [],
+    					"x": 474,
+    					"y": 244,
+    					"z": 0,
+    					"r": 180
+					}
 
 				world.rooms.push({ baseroom: "shack", roomId: id, name: data, width: 855, height: 480, startX: 440, startY: 380, startZ: 0, layout: { playground: [[]], height: 480, width: 885 }, media: { background: "https://i.imgur.com/meyqL7K.png", foreground: "", music: "https://www.dl.dropboxusercontent.com/s/j30mu8q6f6tvkch/29.%20Charlies%20Here.mp3?dl=1", navMesh: "https://i.imgur.com/OD7eE16.png", treasure: "https://i.imgur.com/m3YCOSh.png", }, spritesheet: {}, triggers: [{ hex: 'ff0000', world: { joinRoom: 'port' } }], })
 				client.loadedData.rooms.push({ baseroom: "shack", roomId: id, name: data, width: 855, height: 480, startX: 440, startY: 380, startZ: 0, layout: { playground: [[]], height: 480, width: 885 }, media: { background: "https://i.imgur.com/meyqL7K.png", foreground: "", music: "https://www.dl.dropboxusercontent.com/s/j30mu8q6f6tvkch/29.%20Charlies%20Here.mp3?dl=1", navMesh: "https://i.imgur.com/OD7eE16.png", treasure: "https://i.imgur.com/m3YCOSh.png", }, spritesheet: {}, triggers: [{ hex: 'ff0000', world: { joinRoom: 'port' } }], })
 				world.player.x = world.rooms[world.rooms.length - 1].startX
 				world.player.y = world.rooms[world.rooms.length - 1].startY
-				setTimeout(function () { world.handleJoinRoom({ roomId: world.rooms[world.rooms.length - 1].roomId, playerCrumbs: [client.makeCrumb(world.player)] }) }, 1000)
+				setTimeout(function () { world.handleJoinRoom({ roomId: world.rooms[world.rooms.length - 1].roomId, playerCrumbs: [client.makeCrumb(world.player),crumby] }) }, 1000)
 				world.joinRoom(world.rooms[world.rooms.length - 1].baseroom)
 				this.numid = world.rooms.length - 1
 				this.id = world.rooms[world.rooms.length - 1].roomId
